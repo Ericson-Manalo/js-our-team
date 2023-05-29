99bc1dbcabf88784bd5c99030673b13bd5e04b82
Ericson-Manalo <ericmana98@gmail.com>
2023-05-29 15:51:53 +0200

    added picture of each object

--- js/myscript.js
+++ js/myscript.js
@@ -9,7 +9,8 @@ Stampare le stesse informazioni su DOM sottoforma di stringhe
 BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
-Organizzare i singoli membri in card/schedeConsigli del giorno:
+Organizzare i singoli membri in card/schede
+Consigli del giorno:
 Ragioniamo come sempre a step.
 Prima la logica in italiano e poi traduciamo in codice.*/
 
@@ -20,32 +21,32 @@ const teamMate = [
     {
     name: 'Wayne Barnett',
     role: 'Founder & CEO',
-
+    picture: 'wayne-barnett-founder-ceo.jpg'
     },
     {
     name: 'Angela Caroll',
     role: 'Chief Editor',
-
+    picture: 'angela-caroll-chief-editor.jpg',
     },
-        {
+    {
     name: 'Walter Gordon',
     role: 'Office Manager',
-
+    picture: 'walter-gordon-office-manager.jpg' 
     },
-        {
+    {
     name: 'Angela Lopez',
-    role: 'Social Media',
-
+    role: 'Social Media Manager',
+    picture: 'angela-lopez-social-media-manager.jpg'
     },
-        {
+    {
     name: 'Scott Estrada',
     role: 'Developer',
-
+    picture: 'scott-estrada-developer.jpg'
     },
-        {
+    {
     name: 'Barbara Ramos',
     role: 'Graphic Designer',
-
+    picture: 'barbara-ramos-graphic-designer.jpg'
     },
         
 ];
